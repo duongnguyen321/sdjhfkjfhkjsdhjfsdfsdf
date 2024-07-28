058eaa7cbfd0090d3ecf12b5e3ead97eef4e1820
Nguyễn Dương <duongcoilc2004@gmail.com>
2024-07-29 03:28:45 +0700

    [fixed]: Fix bug requirement and fix mine

--- FE-webmuaxu/vite.config.js
+++ FE-webmuaxu/vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url';
 import dotenv from 'dotenv';
-
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 // Load environment variables from .env file
 dotenv.config();
 
